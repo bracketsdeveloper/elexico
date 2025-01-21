5adf95450665d02479a2398034eb118e1dd0c29b
Bharath1702 <bharathac7@gmail.com>
2025-01-21 21:11:42 +0530

    v-1

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -1,4 +1,4 @@
-const backendDomain = 'http://localhost:8080' ||'https://prakritioils-api.vercel.app' //  
+const backendDomain = 'https://elexico.vercel.app'||'http://localhost:8080' //  
 const SummaryApi = {
     signUp : {
         url : `${backendDomain}/api/signup`,
